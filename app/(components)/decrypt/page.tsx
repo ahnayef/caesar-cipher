5da09099b4ac56ae893534a833f1d47f0031dff4
AHNayef <ahnayef@outlook.com>
2023-11-04 22:38:03 +0600

    Add decryption functionality

--- app/(components)/decrypt/page.tsx
+++ app/(components)/decrypt/page.tsx
@@ -1,12 +1,59 @@
 "use client"
 
+import { useEffect, useState } from "react"
 import style from "./decrypt.module.css"
 
+const initialState: { encodedText: string, decodedText: string, key:number } = {
+  encodedText: "",
+  decodedText: "",
+  key: 0
+}
 
-export default function Page() {
+
+function decodeText(text: string, key:number) {
+  return text.split("").map((char: string) => {
+    if (char >= 'A' && char <= 'Z') {
+      return String.fromCharCode(((char.charCodeAt(0) - 65 - key) % 26) + 65);
+    } else if (char >= 'a' && char <= 'z') {
+      return String.fromCharCode(((char.charCodeAt(0) - 97 - key) % 26) + 97);
+    } else {
+      return char;
+    }
+  }).join("");
+}
+
+export default function Page({  searchParams, }: { searchParams?: { [key: string]: string | string[] | undefined } }) {
+
+  const [formState, setFormState] = useState(initialState);
+
+  const encodedText = searchParams?.text;
+
+  useEffect(() => {
+    if (encodedText) {
+      setFormState({ ...formState, encodedText: encodedText as string });
+    }
+  }, [])
+
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
+
+  const decode = (e: React.FormEvent) => {
+    e.preventDefault();
+    const encodedText = formState.encodedText;
+    setFormState({ ...formState, decodedText: decodeText(encodedText,formState.key) });
+  }
+
   return (
-    <div className={style.decrypt}>
-      
+    <div className={style.decryptMain}>
+      <form onSubmit={decode}>
+        <input type="text" name="encodedText" onChange={handleChange} value={formState.encodedText} />
+        <input type="number" placeholder="Enter key" name="key" onChange={handleChange} required />
+        <button type="submit">Decrypt</button>
+        <p>{formState.decodedText}</p>
+
+      </form>
     </div>
   )
 }
