a8147d972e75d901182c8b10a9b185ffc04878b4
AHNayef <ahnayef@outlook.com>
2023-11-04 08:40:03 +0600

    Auth page

---
+++ app/(components)/auth/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import style from "./auth.module.css"
+
+export default function Page() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
+
+    return (
+        <div className={style.authMain}>
+            <form onSubmit={handleSubmit}>
+                <h1>Hello</h1>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
